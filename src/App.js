d479ccb7b1ccfb195a1ccea4ecce440c9ce8756c
MacOs <macos@MacOss-iMac.local>
2018-08-24 15:05:28 +0400

    add 15 files

--- src/App.js
+++ src/App.js
@@ -12,7 +12,20 @@ import Music from './Music';
 import Paella from './Paella';
 import Chips from './Chips';
 import Divider from './Divider';
-import Drawer from './Drawer';
+//import Drawer from './Drawer';
+import Accordion from './Accordion';
+//import ImageGrid from './ImageGrid';
+import NestedList from './NestedList';
+import Wifi from './Wifi';
+import DotMenu from './DotMenu';
+import SelectMenu from './SelectMenu';
+import Notification from './Notification';
+import Paper from './Paper';
+import Date from './Date';
+import CircleProgress from './CircleProgress';
+import LinearProgress from './LinearProgress';
+import Checkbox from './Checkbox';
+import CustomizedSwitches from './CustomizedSwitches';
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 
@@ -47,7 +60,32 @@ class App extends Component {
         <div>
           <Divider/>
         </div>
-        <Drawer/>
+        {/* Drawer   requires tiledata */}
+        <Accordion/>
+        {/* ImageGrid  requires tiledata  */}
+        <Wifi/>
+        <NestedList/>
+        <div className={classes.dot}>
+          <DotMenu/>
+          <SelectMenu/>
+          <Notification/>
+        </div>
+        <Paper/>
+        <div className={classes.date}>
+          <Date/>
+        </div>
+        <div className={classes.progress}>
+          <CircleProgress/>
+        </div>
+        <div className={classes.progress}>
+          <LinearProgress/>
+        </div>
+        <div>
+          <Checkbox/>
+        </div>
+        <div>
+          <CustomizedSwitches/>
+        </div>
       </div>
     );
   }
@@ -75,7 +113,18 @@ const styles = theme => ({
   space:{
     marginTop:30,
     marginBottom:30
-  }
+  },
+  dot:{
+    textAlign:"left",
+    display: "flex",
+    justifyContent: 'center'
+  },
+  date:{
+    margin:60
+  },
+  progress:{
+    margin:50
+  },
 });
  
 
